e526d4711336fdff3e21840b37586b70cee96424
kasumi <zils04@163.com>
2022-06-12 21:23:54 +0800

    add: 部署到云服务器配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 由于webpack打包工具会默认将以为项目会部署在根路径下 , 请求资源都是以根路径为主 , 添加这行代码是为了 , 请求js或css等资源不被404
+  // publicPath: process.env.NODE_ENV === 'production' ? '/仓库地址/' : './'
+  publicPath: process.env.NODE_ENV === 'production' ? '/event-deployment/' : './'
+  // 需要添加.spa让pages对单页面支持
 })
